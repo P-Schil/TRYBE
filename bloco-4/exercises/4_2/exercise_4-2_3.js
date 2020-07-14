00fd676817b90f19d28f20fe5935586914cbd6b4
Pedro Schil <schil.ambiental@gmail.com>
2020-07-14 15:39:23 -0300

    exercise 3. 4-2

---
+++ bloco-4/exercises/4_2/exercise_4-2_3.js
@@ -0,0 +1,11 @@
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let sum =0;
+let arithmeticAvarage;
+
+for (let i=0;i<10;i++){;
+sum=sum+numbers[i];
+}
+arithmeticAvarange = sum/numbers.length;
+console.log(arithmeticAvarange);
