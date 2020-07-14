552e0b3e0a9fc81c434ae9a3dbace587525872b8
Pedro Schil <schil.ambiental@gmail.com>
2020-07-14 15:28:51 -0300

    exercise 2, 4-2

---
+++ bloco-4/exercises/4_2/exercise_4-2_2.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma=0;
+for ( let i = 0; i<10 ; i++){;
+soma=soma+(numbers[i]);
+}
+console.log(soma);
+
+//Para o segundo exercício, você deve somar todos os 
+//valores contidos no array e imprimir o resultado;
