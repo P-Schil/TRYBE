7d9ac5f58e99718101fd48f0be33fb3fc9dac831
Pedro Schil <schil.ambiental@gmail.com>
2020-07-14 15:15:34 -0300

    exercise 1, 4-2

---
+++ bloco-4/exercises/4_2/exercise_4-2_1.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for ( let i =0;i<10;i++){
+    console.log(numbers[i])
+}
+
+//Nesse primeiro exercício, percorra o array imprimindo 
+//todos os valores nele contidos com a função console.log();
