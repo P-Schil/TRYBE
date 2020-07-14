1f7234031a1deafcbedc7e0646181215a9150069
Pedro Schil <schil.ambiental@gmail.com>
2020-07-14 18:49:24 -0300

    exercise 2, 4-3

---
+++ bloco-4/exercises/4_3/exercises_4-3_2.js
@@ -0,0 +1,11 @@
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+//Por exemplo:
+
+let n=5;
+let asteriskLine="";
+
+
+for(let i=0;i<n;i++){
+    asteriskLine=asteriskLine+"*"
+    console.log(asteriskLine)
+}
