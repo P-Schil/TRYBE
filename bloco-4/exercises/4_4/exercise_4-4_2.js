7e1902654ebaf43d5d1d1f6f77a4bca79411a082
Pedro Schil <schil.ambiental@gmail.com>
2020-07-16 15:17:04 -0300

    exercise 2, 4-4

---
+++ bloco-4/exercises/4_4/exercise_4-4_2.js
@@ -0,0 +1,14 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+  };
+
+for (let i in info){
+    console.log(i,info[i])
+}
+
+
+  //Insira no objeto uma nova propriedade com o nome de chave "recorrente" e 
+  // o valor "Sim" e, em seguida, imprima o objeto no console.
