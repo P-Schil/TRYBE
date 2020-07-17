cae35e788e6811472fe3e0d10f5e3c4b5f557b3c
Pedro Schil <schil.ambiental@gmail.com>
2020-07-17 00:12:42 -0300

    exercicio 6 e esboço do 5 -p2, 4

---
+++ bloco-4/exercises/4_4/part2/exercise_4-4_p2_5.js
@@ -0,0 +1,12 @@
+/*Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+Valor esperado no retorno da função: 2.*/
+
+function mostRepeatedInteger (array){
+    let arrayRepeated =[]
+    for(let i=0;i<array.length;i++){
+        for(let k=i;k<array.length;k++){
+            if(array[k]=)
+        }
+    }
+}
